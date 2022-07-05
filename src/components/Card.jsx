cba23f57ae8d4bdacc7d68ec00f434faa8859f29
shaheersystems <105648279+shaheersystems@users.noreply.github.com>
2022-07-05 14:33:30 +0500

    Just a word change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card() {
   return (
     <div className="card">
-      <h2>Love is ...</h2>
+      <h2>Love is Dagger</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quis ut
         neque deleniti sequi vitae ab delectus nemo quaerat perspiciatis atque
